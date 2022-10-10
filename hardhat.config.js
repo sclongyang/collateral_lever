b3001ea76a0fed5dd108ff7b51efdbe6f7e3cf57
sclongyang@126.com <sclongyang@126.com>
2022-10-10 16:42:43 +0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,77 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const MAINNET_RPC_URL = process.env.ALCHEMY_MAINNET_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const REPORT_GAS = process.env.REPORT_GAS || false
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        // hardhat: {            
+        //     forking: {
+        //       url: MAINNET_RPC_URL
+        //     }
+        //     chainId: 31337,
+        // },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 5,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 1,
+        },        
+    },
+    etherscan: {        
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,            
+        },
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,        
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: [""],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, 
+            1: 0, 
+        },
+        user: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000, // 200s
+    },
+}
